4d0cd0f1ee478063b797eb0385d3158b775a694d
Ameer <ameerapex496@gmail.com>
2023-12-14 07:59:46 +0100

    added comments, and selected elements needed

--- src/script.js
+++ src/script.js
@@ -1,1 +1,10 @@
-console.log("hallo world");
+// selecting elements
+var rangeTimer = document.getElementsByClassName("range-Timer")[0];
+var timerEl = document.getElementsByClassName("timer-El")[0];
+var textH1 = document.getElementsByClassName("text-H1")[0];
+var textImg = document.getElementsByClassName("text-Img")[0];
+var nameP = document.getElementsByClassName("name-P")[0];
+// gelobal variables
+// our Object Data
+// our functions
+// our eventlisnters
--- src/script.ts
+++ src/script.ts
@@ -1,1 +1,14 @@
-console.log("hallo world");
+// selecting elements
+const rangeTimer = document.getElementsByClassName("range-Timer")[0];
+const timerEl = document.getElementsByClassName("timer-El")[0];
+const textH1 = document.getElementsByClassName("text-H1")[0];
+const textImg = document.getElementsByClassName("text-Img")[0];
+const nameP = document.getElementsByClassName("name-P")[0];
+
+// gelobal variables
+
+// our Object Data
+
+// our functions
+
+// our eventlisnters
